3b8caeab21eb4b81521b1579abd5854a656f3734
Tatyana Abramova <tanusha-atv@mail.ru>
2022-07-11 10:03:17 +0500

    add Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество чисел чисел М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int CompNumber(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+Console.WriteLine($"Положительных чисел: {CompNumber(massiveNumbers)} ");
---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найти точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[,] Coff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCofficients()
+{
+  for (int i = 0; i < Coff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < Coff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k{i+1}: ");
+      else Console.Write($"Введите коэффициент b{i+1}: ");
+      Coff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] Coff)
+{
+  crossPoint[0] = (Coff[1,1] - Coff[0,1]) / (Coff[0,0] - Coff[1,0]);
+  crossPoint[1] = crossPoint[0] * Coff[0,0] + Coff[0,1];
+  return crossPoint;
+}
+
+void OutputResponse(double[,] Coff)
+{
+  if (Coff[0,0] == Coff[1,0] && Coff[0,1] == Coff[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (Coff[0,0] == Coff[1,0] && Coff[0,1] != Coff[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Decision(Coff);
+    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}; {crossPoint[1]})");
+  }
+}
+
+InputCofficients();
+OutputResponse(Coff);
